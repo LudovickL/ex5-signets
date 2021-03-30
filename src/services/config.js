a1cc688f44cceef7ddfc47b39c74592255882a16
LudovickL <Luberisselud@gmail.com>
2021-03-30 15:10:48 -0400

    date en ordre récent à ancient + supprimer doc

--- src/services/config.js
+++ src/services/config.js
@@ -5,7 +5,14 @@
 
 // Configuration Firebase (mettez-y les vôtres !)
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyBOlzfI2KFofrzdabWQGNDnnNaBd0VIE_U",
+  authDomain: "pvt-h21-luberisse-ludovick.firebaseapp.com",
+  databaseURL: "https://pvt-h21-luberisse-ludovick-default-rtdb.firebaseio.com",
+  projectId: "pvt-h21-luberisse-ludovick",
+  storageBucket: "pvt-h21-luberisse-ludovick.appspot.com",
+  messagingSenderId: "148589257484",
+  appId: "1:148589257484:web:6911a255da88076cc7df29",
+  measurementId: "G-7E4KNNT45H"
 };
 export default firebaseConfig;
 
